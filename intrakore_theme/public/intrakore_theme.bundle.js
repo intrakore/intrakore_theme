124d425eff36776bb55de395d6532ef858a29048
intrakore <intrakore@gmail.com>
2026-05-06 11:04:20 +0530

    Added correct Hooks

--- intrakore_theme/public/intrakore_theme.bundle.js
+++ intrakore_theme/public/intrakore_theme.bundle.js
@@ -1,6 +1,17 @@
 import "./js/Button.js";
 import "./js/Alert.js";
+import "./js/Badge.js";
+import "./js/Breadcrumbs.js";
+import "./js/Calendar.js";
+import "./js/Checkbox.js";
+import "./js/Divider.js";
 import "./js/IconPicker.js";
 import "./js/List.js";
+import "./js/Rating.js";
 import "./js/Sidebar.js";
+import "./js/Toast.js";
+import "./js/Tooltip.js";
+import "./js/Tree.js";
+
+import "./js/component_overrides.js";
 import "./js/custom.js";
